4e885926a6aafd883739b307f0f06b078e469742
takashi-ebina <takashi.ebina.88@gmail.com>
2026-05-10 11:21:36 +0900

    Hello Worldのサンプルコードの更新

--- reactor-demo/src/main/java/reactor/demo/App.java
+++ reactor-demo/src/main/java/reactor/demo/App.java
@@ -9,11 +9,11 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        // List から要素を順に発行する「cold」なFluxを作成する
-        Flux<String> flux = Flux.just("HELLO ", "WORLD ", "DEMO!");
+        // List から要素を順に発行するFluxを作成する
+        Flux<String> flux = Flux.just("HELLO", " ", "WORLD", "!"); // [1]
         // Operator: 要素を順に小文字に変換する新しいFluxを作成する
-        Flux<String> lowerCaseFlux = flux.map(String::toLowerCase);
-        // subscribe: 購読開始して、Flux が値を発行すれば println が呼ばれる
-        lowerCaseFlux.subscribe(System.out::print); // 出力: "hello world demo!"
+        Flux<String> lowerCaseFlux = flux.map(String::toLowerCase); // [2]
+        // subscribe: 購読開始して、Flux が値を発行すれば print が呼ばれる
+        lowerCaseFlux.subscribe(System.out::print); // [3]
     }
 }
